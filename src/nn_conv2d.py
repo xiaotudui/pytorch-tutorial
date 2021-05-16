742d284a1269554594ed989a9cd4c4ee2fb3890a
xiaotudui <zhiyaozhou@126.com>
2021-05-16 14:25:11 +0800

    add conv2d layer

---
+++ src/nn_conv2d.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# 作者：小土堆
+# 公众号：土堆碎念
+import torch
+import torchvision
+from torch import nn
+from torch.nn import Conv2d
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+dataset = torchvision.datasets.CIFAR10("../data", train=False, transform=torchvision.transforms.ToTensor(),
+                                       download=True)
+dataloader = DataLoader(dataset, batch_size=64)
+
+class Tudui(nn.Module):
+    def __init__(self):
+        super(Tudui, self).__init__()
+        self.conv1 = Conv2d(in_channels=3, out_channels=6, kernel_size=3, stride=1, padding=0)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        return x
+
+tudui = Tudui()
+
+writer = SummaryWriter("../logs")
+
+step = 0
+for data in dataloader:
+    imgs, targets = data
+    output = tudui(imgs)
+    print(imgs.shape)
+    print(output.shape)
+    # torch.Size([64, 3, 32, 32])
+    writer.add_images("input", imgs, step)
+    # torch.Size([64, 6, 30, 30])  -> [xxx, 3, 30, 30]
+
+    output = torch.reshape(output, (-1, 3, 30, 30))
+    writer.add_images("output", output, step)
+
+    step = step + 1
+
+
