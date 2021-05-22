facb521475321800c61a6b5c68f355bc47deb0bd
xiaotudui <zhiyaozhou@126.com>
2021-05-22 10:44:45 +0800

    add nn_maxpool.py and update maxpool chapter in PPT

---
+++ src/nn_maxpool.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# 作者：小土堆
+# 公众号：土堆碎念
+
+import torch
+import torchvision
+from torch import nn
+from torch.nn import MaxPool2d
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+dataset = torchvision.datasets.CIFAR10("../data", train=False, download=True,
+                                       transform=torchvision.transforms.ToTensor())
+
+dataloader = DataLoader(dataset, batch_size=64)
+
+class Tudui(nn.Module):
+    def __init__(self):
+        super(Tudui, self).__init__()
+        self.maxpool1 = MaxPool2d(kernel_size=3, ceil_mode=False)
+
+    def forward(self, input):
+        output = self.maxpool1(input)
+        return output
+
+tudui = Tudui()
+
+writer = SummaryWriter("../logs_maxpool")
+step = 0
+
+for data in dataloader:
+    imgs, targets = data
+    writer.add_images("input", imgs, step)
+    output = tudui(imgs)
+    writer.add_images("output", output, step)
+    step = step + 1
+
+writer.close()
