aa8dc6600c96d08304332a03966bdff5064b1239
xiaotudui <zhiyaozhou@126.com>
2021-05-29 16:57:43 +0800

    add model_pretrained.py

---
+++ src/model_pretrained.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# 作者：小土堆
+# 公众号：土堆碎念
+import torchvision
+
+# train_data = torchvision.datasets.ImageNet("../data_image_net", split='train', download=True,
+#                                            transform=torchvision.transforms.ToTensor())
+from torch import nn
+
+vgg16_false = torchvision.models.vgg16(pretrained=False)
+vgg16_true = torchvision.models.vgg16(pretrained=True)
+
+print(vgg16_true)
+
+train_data = torchvision.datasets.CIFAR10('../data', train=True, transform=torchvision.transforms.ToTensor(),
+                                          download=True)
+
+vgg16_true.classifier.add_module('add_linear', nn.Linear(1000, 10))
+print(vgg16_true)
+
+print(vgg16_false)
+vgg16_false.classifier[6] = nn.Linear(4096, 10)
+print(vgg16_false)
+
+
--- src/train.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# 作者：小土堆
-# 公众号：土堆碎念
-import torch
-import torchvision
